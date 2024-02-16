e2d94bfe2c8856b2b9f3d50cbbd41a1a5deba939
haripritamreddy <114937056+Haripritamreddy@users.noreply.github.com>
2024-02-16 19:03:50 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
 import nltk
+import os
+
+# Set nltk data directory to a directory in your home folder
+home = os.path.expanduser("~")
+nltk_data_dir = os.path.join(home, "nltk_data")
+if not os.path.exists(nltk_data_dir):
+    os.mkdir(nltk_data_dir)
+nltk.data.path.append(nltk_data_dir)
+
 nltk.download('stopwords')
+
 import streamlit as st
 from llama_index.core import VectorStoreIndex, ServiceContext, Document
 from llama_index.embeddings.google import GooglePaLMEmbedding
